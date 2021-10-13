6311fdf448ac8fed79241917a82aa017087a5891
siba98 <gyansiba.patro@gmail.com>
2021-10-13 21:34:53 +0530

    Siba:[Added] Checking Two Strings are Anagram or Not

---
+++ Day16_17_Algorithm_Assignments/Anagram/Anagram_String.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day16_17_Algorithm_Assignments
+{
+	class Anagram_String
+	{
+		public static void IsAnagram()
+		{
+            Console.Write("Enter first word: ");
+            string word1 = Console.ReadLine();
+            Console.Write("Enter second word: ");
+            string word2 = Console.ReadLine();
+
+            char[] char1 = word1.ToLower().ToCharArray();
+            char[] char2 = word2.ToLower().ToCharArray();
+
+            Array.Sort(char1);
+            Array.Sort(char2);
+
+            string NewWord1 = new string(char1);
+            string NewWord2 = new string(char2);
+
+            if (NewWord1 == NewWord2)
+            {
+                Console.WriteLine("Yes! Words \"{0}\" and \"{1}\" are Anagrams", word1, word2);
+            }
+            else
+            {
+                Console.WriteLine("No! Words \"{0}\" and \"{1}\" are not Anagrams", word1, word2);
+            }
+        }
+    }
+}
